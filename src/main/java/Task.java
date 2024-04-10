315b6770f8a5e3f53572e75272b9ec95d338dfcb
Diggins <Dylanhabis@yahoo.com>
2024-04-10 12:52:51 +0100

    Added need methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Task {
         private String Owner;
@@ -10,15 +11,60 @@ public class Task {
         Description = description;
         this.deadline = deadline;
 
+        //TODO ADD GOOD VALIDATION
+
         if (deadline.equals(LocalDate.now()))
         {
             System.out.println("UR LATE U GET 0 LMAFOOOOOOOOOOOOO");
         }
     }
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    //TODO; check if right
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(Owner, task.Owner) && Objects.equals(Description, task.Description) && Objects.equals(deadline, task.deadline);
+    }
+
+        //TODO; check if right
+    @Override
+    public int hashCode() {
+        return Objects.hash(Owner, Description, deadline);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "Owner='" + Owner + '\'' +
+                ", Description='" + Description + '\'' +
+                ", deadline=" + deadline +
+                '}';
+    }
 }
-
-//////
-////////////////////////////////
-/////////
-
-
