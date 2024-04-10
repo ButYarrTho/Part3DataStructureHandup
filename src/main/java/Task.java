4882481d7e8f77e1aa31b93a886d484aae747fc6
alexc <alexclinton133@gmail.com>
2024-04-10 12:33:46 +0100

    Created new package

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,10 +2,8 @@ import java.time.LocalDate;
 
 public class Task {
 
-
         private String Owner;
         private String  Description;
-
         private LocalDate deadline = LocalDate.of(2024, 4, 16);
     }
 
