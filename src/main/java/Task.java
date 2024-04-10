22178bdf43d771629a16b5c433fcee1cc38103f8
Diggins <Dylanhabis@yahoo.com>
2024-04-10 12:41:23 +0100

    Added Task.java variables

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,21 @@
 import java.time.LocalDate;
 
 public class Task {
-
         private String Owner;
         private String  Description;
         private LocalDate deadline = LocalDate.of(2024, 4, 16);
+
+    public Task(String owner, String description, LocalDate deadline) {
+        Owner = owner;
+        Description = description;
+        this.deadline = deadline;
+
+        if (deadline.equals(LocalDate.now()))
+        {
+            System.out.println("UR LATE U GET 0 LMAFOOOOOOOOOOOOOO");
+        }
     }
+}
 
 
 
